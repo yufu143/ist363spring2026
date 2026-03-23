7a25d13dd6dc6e4b26a42c7bd709ff65930c69d4
yufu143 <yfu143@syr.edu>
2026-03-23 18:13:08 -0400

    ooooo

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Card1 from './components/card1.jsx'
 import './App.css'
 
 function App() {
