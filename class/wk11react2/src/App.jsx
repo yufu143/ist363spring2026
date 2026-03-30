cf0eaee7c4236992e019500b1a70f0adf4e3fc5a
yufu143 <yfu143@syr.edu>
2026-03-30 18:18:47 -0400

    Update App.jsx

--- class/wk11react2/src/App.jsx
+++ class/wk11react2/src/App.jsx
@@ -1,41 +1,52 @@
 import { useState } from "react";
 
 const students = [
-  {suid: 123456, name: 'Sue Flay', year: 'senior', major: 'Applied Data Analytics'}, 
-  {suid: 234567, name: 'Ella Vader', year: 'junior', major: 'Information Management and Technology'}, 
-  {suid: 345678, name: 'Chris P Bacon', year: 'junior', major: 'Innovation, Society and Technology'}
+  { suid: 123456, name: 'Sue Flay', year: 'senior', major: 'Applied Data Analytics' },
+  { suid: 234567, name: 'Ella Vader', year: 'junior', major: 'Information Management and Technology' },
+  { suid: 345678, name: 'Chris P Bacon', year: 'junior', major: 'Innovation, Society and Technology' }
 ];
 
 function App() {
   let [filteredStudents, setFilteredStudents] = useState(students);
-   
+
   const handleChange = (event) => {
     setFilteredStudents(
-      students.filter(student => 
+      students.filter(student =>
         student.name.toLowerCase().includes(event.target.value.toLowerCase())
       )
-    );}
-  
-  return (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onChange={handleChange} />
-    <h1>Students</h1>
-    <ul>
-  {filteredStudents.map(function (item) {
-    return (
-      <li key={item.suid}>
-        Name: {item.name}
-        <br />
-        Year: {item.year}
-        <br />
-        Major: {item.major}
-      </li>
     );
-  })}
-</ul>
-  </div>
-      );
-    };  
+  }
+
+  const Liked = () => {
+    const [liked, setLiked] = useState('whiteheart');
+  }
+
+  return (
+
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <h1>Students</h1>
+      <ul>
+        {filteredStudents.map(function (item) {
+          return (
+            <li key={item.suid}>
+              Name: {item.name}
+              <br />
+              Year: {item.year}
+              <br />
+              Major: {item.major}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+
+//     <div onClick={handleLike}>
+//         <img src="https://picsum.photos/200" />
+//          <p>{liked}</p>
+//         </div>
+  );
+};
 
 export default App;
