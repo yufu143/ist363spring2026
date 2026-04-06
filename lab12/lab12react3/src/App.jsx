7586b24f0070c606fb49496c308a10ca6d3ff079
yufu143 <yfu143@syr.edu>
2026-04-06 18:16:13 -0400

    Update App.jsx

--- lab12/lab12react3/src/App.jsx
+++ lab12/lab12react3/src/App.jsx
@@ -3,84 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import About from './About';
 
-
-let initialTasks = [
-    {id: 1,
-      item: "Complete Lab 11",
-      status: "not completed"
-    },
-    
-    {id: 2,
-      item: "JSX Events & State",
-      status: "not completed"
-    }
-  ]
-
+function Home () {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  
-
+  const [page, setPage] = useState('home');
   return (
-    <>
-      <section id="center">
+    < >
+      
+      <button onClick={() => setPage('home')}>Home</button>
+      <button onClick={() => setPage('about')}>About</button>
 
-        <div>
-          <h1>To-Do List</h1>
-          <ul>
-            {
-              initialTasks.map(function(item) {
-                return <li>
-                    <p>{item.item}</p>
-                  </li>;
-              })
-            }
-          </ul>
-        </div>
-        
-            {/* <i class="fa-solid fa-circle-xmark" style="color: rgb(220, 26, 26);"></i> */}
-            {/* text-decoration: line-through */}
-
-        {/* <div>
-          <h1>Get started</h1>
-          <h3>Student Name</h3>
-          <p>{myName}</p>
-          <div>
-              <ul>
-                <li>{myName}</li>
-                <li>{schoolYear}</li>
-                <li>{myMajor}</li>
-              </ul>
-          </div>
-          <div>
-              <ul>
-                <li>{person.myName}</li>
-                <li>{person.schoolYear}</li>
-                <li>{person.myMajor}</li>
-              </ul>
-          </div>
-
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div> */}
-
-        {/* <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button> */}
-      </section>
-
-      <div className="ticks"></div>
-
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      {page === 'home' && <Home />}
+      {page === 'about' && <About />} 
     </>
-  )
+  );
 }
-export default App
+
+export default App;
