6401b9c2b31a0d29aa21d61a4958b4dafe1c0ebd
yufu143 <yfu143@syr.edu>
2026-04-12 22:20:46 -0400

    Update App.jsx

--- lab12/lab12react3/src/App.jsx
+++ lab12/lab12react3/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Pioneer1 from './components/Pioneer1.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
